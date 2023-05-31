7f21b76fd48bfa41467aac34d5b62c14d1c7e07a
gusehdqkd <gusehdqkd@naver.com>
2023-05-31 17:13:23 +0900

    Test3

--- vars/commonUtils.groovy
+++ vars/commonUtils.groovy
@@ -1,15 +1,19 @@
 def getJobType() {
     String commitMsg = sh(script : "git show -s --format=%B ${env.GIT_COMMIT}", returnStdout: true)
+    def buildType = [
+            build : "",
+            deploy : "",
+            test : ""
+    ]
 
     int beginIdx = commitMsg.indexOf('{')
     int endIdx = commitMsg.indexOf('}')
-    if (beginIdx == -1 || endIdx == -1) return "default"
+    if (beginIdx == -1 || endIdx == -1) return buildType
 
     String tasks = commitMsg.substring(beginIdx + 1, endIdx)
-    def buildType = [
-            build : tasks.contains("BD"),
-            deploy : tasks.contains("DP"),
-            test : tasks.contains("TST")
-    ]
+    buildType.build = tasks.contains("BD")
+    buildType.deploy = tasks.contains("DP")
+    buildType.test = tasks.contains("TST")
+
     return buildType
 }
