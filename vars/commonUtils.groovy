990b8dbde40de7f9d8ef7a8f11e2a605212448bb
gusehdqkd <gusehdqkd@naver.com>
2023-05-31 16:41:25 +0900

    Test - 문자열 가공 테스트 {BD,TST,DP}

--- vars/commonUtils.groovy
+++ vars/commonUtils.groovy
@@ -1,8 +1,7 @@
 def getJobType() {
     script {
         String commitMsg = sh(script : "git show -s --format=%B ${env.GIT_COMMIT}", returnStdout: true)
-                            .trim()
-                            .replace(' ', '-spc-')
+
         int beginIdx = commitMsg.indexOf('{')
         int endIdx = commitMsg.indexOf('}')
         if (beginIdx == -1 || endIdx == -1) return "default"
