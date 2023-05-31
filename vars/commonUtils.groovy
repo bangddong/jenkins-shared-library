8e3ddd3cccc5d7be343780df5c2ce69913021c78
gusehdqkd <gusehdqkd@naver.com>
2023-05-31 17:05:19 +0900

    Test - 분기처리 테스트중2 {BD,DP,TST}

--- vars/commonUtils.groovy
+++ vars/commonUtils.groovy
@@ -1,10 +1,15 @@
 def getJobType() {
-    script {
-        String commitMsg = sh(script : "git show -s --format=%B ${env.GIT_COMMIT}", returnStdout: true)
+    String commitMsg = sh(script : "git show -s --format=%B ${env.GIT_COMMIT}", returnStdout: true)
 
-        int beginIdx = commitMsg.indexOf('{')
-        int endIdx = commitMsg.indexOf('}')
-        if (beginIdx == -1 || endIdx == -1) return "default"
-        return commitMsg.substring(beginIdx + 1, endIdx)
-    }
+    int beginIdx = commitMsg.indexOf('{')
+    int endIdx = commitMsg.indexOf('}')
+    if (beginIdx == -1 || endIdx == -1) return "default"
+
+    String tasks = commitMsg.substring(beginIdx + 1, endIdx)
+    def buildType = [
+            build : tasks.contains("BD"),
+            deploy : tasks.contains("DP"),
+            test : tasks.contains("TST")
+    ]
+    return buildType
 }
